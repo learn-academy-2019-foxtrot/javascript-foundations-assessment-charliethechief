5d278833b154a8ede39fa8961e5ac345e82b5607
Ubuntu <ubuntu@ip-172-31-33-18.us-east-2.compute.internal>
2019-12-02 19:24:37 +0000

    completed assessment

--- js-foundations.js
+++ js-foundations.js
@@ -3,11 +3,24 @@
 
 // 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
 
-
+const divider = (x) => {
+    
+if (x %3=== 0){  
+    return x +" is divisable"
+}else{
+    return x + " is not divisable"
+    }
+}
+console.log(divider(15))
+console.log(divider(0))
+console.log(divider(385))
+console.log(divider(-7))
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
 
-
+var helloMe = ["Hi my name is Charlie","Montejo","I am a student","that is 21 years old"]
+var helloMeSen = helloMe.join(" ")
+console.log (helloMeSen)
 
 // ------------------------------------- Consider the variable:
 
@@ -15,11 +28,21 @@ var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "tem
 
 // 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
 
-
-
+const arrFilter = (array) => {
+   let arrFilter = array.filter((value,index) => {
+       return index%2 === 0
+   })
+   return arrFilter.join (" ")
+}
+   console.log  (arrFilter(randomNouns))
+   
 // 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
-
+var str = randomNouns.join()
+function cap() {
+    return  str.toUpperCase().split(", ")
+} 
+console.log(cap(randomNouns))
 
 // 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
 
@@ -32,12 +55,22 @@ var testString2 = "academy"
 var testString3 = "sandiego"
 
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
-
-
+var tS1 = testString1.split("").sort()
+var tS2 = testString2.split("").sort()
+var tS3 = testString3.split("").sort()
+const alphabetSoup = (x) =>{
+    return x
+}
+console.log(alphabetSoup(tS1).join(""))
+console.log(alphabetSoup(tS2).join(""))
+console.log(alphabetSoup(tS3).join(""))
 
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
-
-
+const alphabetSoup2 = (...strings) => {
+    let alphabetSoup3 = [...strings]
+    return strings.join("").split("").sort().join("")
+}
+console.log (alphabetSoup2(testString1,testString2,testString3))
 
 // ------------------------------------- Consider the variables:
 var amounts = [9, 1, 8, 16, 5, 1, 42]
@@ -45,6 +78,12 @@ var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguin
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
 
-
+let combine = (array1,array2) => {
+    let combined = array1.map((value,index) => {
+    return value + " " + array2[index]
+    })
+    return combined.join(" ")
+}
+console.log(combine(amounts,animals))
 
 // 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
--- js-foundations.md
+++ js-foundations.md
@@ -15,42 +15,56 @@ logIt()
 
 1a. Look at this Javascript function and try to predict what the console show.
 
-
+Nothing because the console.log needs to be outside of the function
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer: It returned undefined. I think it was because the log had nothing to attach to
 
-  Researched answer:
+  Researched answer:The log() method returns the natural logarithm (base E) of a number.
+
+
 
 
 2. What is JSON?
 
   Your answer:
 
-  Researched answer:
+  Researched answer: JSON is text, and we can convert any JavaScript object into JSON, and send JSON to the server. We can also convert any JSON received from the server into JavaScript objects.
+
+This way we can work with the data as JavaScript objects, with no complicated parsing and translations.
+
+
 
 
 3. What does CRUD stand for?
 
-  Your answer:
+  Your answer: It's an acronym that is used in web development, most programs have CRUD rules and constraints.
 
-  Researched answer:
+  Researched answer:(C)reate command. If you pull up a saved table and read the details, you are (R)etrieving the data. If you go a step further and then change the details within a field of the table, 
+  you are using the (U)date command. When a field within the table is no longer needed, you can prompt the program to (D)elete that field.
 
 
 
 4. What does are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: They are used to preform actions in HTML 
 
-  Researched answer:
+  Researched answer:HTTP provides methods (verbs) for the actions performed on a response. HTTP verbs are used on an action method. HTTP provides the following main verbs.
+Get
+Post
+Put
+Delete
+
 
 
 5. What is a higher-order function?
 
-  Your answer:
+  Your answer: a higher order function is a simplfied version of a function that can take multiple functions as an argument 
 
-  Researched answer:
+  Researched answer:In mathematics and computer science, a higher-order function is a function that does at least one of the following:
+takes one or more functions as arguments (i.e. procedural parameters),
+returns a function as its result.
 
 
 6. STRETCH: What is a closure, what is it good for and how do you recognize one?
@@ -71,10 +85,11 @@ logIt()
 
 1. Do you have a suggestion for a Check In question?
 
-
+Favorite song?
 
 2. What was your favorite topic this week?
-
+CSS
 
 
 3. What was your "A-ha!" moment this week?
+Working on the magic 8ball and figuring out how to make an animation to run.
